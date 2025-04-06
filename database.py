7f44ecd9efeefc0d009b62dd2fdcc13a90871b46
Prannav <prannavragu@gmail.com>
2025-04-06 18:31:57 +0530

    final

--- database.py
+++ database.py
@@ -315,7 +315,7 @@ def get_user_stats(user_id: int) -> List[Tuple[int, bool]]:
     """Get user stats including points"""
     try:
         # Get points
-        response = supabase.table('points').select('points').eq('username', f'<@{user_id}>').execute()
+        response = supabase.table('points').select('user_points').eq('username', f'<@{user_id}>').execute()
         points = response.data[0]['points'] if response.data else 0
         
         return [(points,)]
